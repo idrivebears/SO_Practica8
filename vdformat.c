e76e81ffe6df352f5a6fc90bad411233bd35d4e8
idrivebears <idrivebears@gmail.com>
2017-05-12 16:01:19 -0500

    vdformat allegedly done

--- vdformat.c
+++ vdformat.c
@@ -5,10 +5,23 @@
 #include "filesystem.h"
 
 int main(int argc,char *argv[])
-{
-    char *buffer;
-    int re = vdwriteseclog(0, buffer);
+{   
     printf("done");
 
+    //Escribir mapa de nodos i
+    char buffer[512];
+    memset(&buffer[0], 0x00, sizeof(mapa_nodosi));
+
+    memcpy(buffer, &mapa_nodosi, sizeof(mapa_nodosi));
+    int re = vdwriteseclog(2, buffer);
+
+    re += vdwriteseclog(3, buffer);
+    re += vdwriteseclog(4, buffer);
+    re += vdwriteseclog(5, buffer);
+    if(re == 4)
+        printf("Disk format completed successfuly.\n");
+    else
+        printf("DISK FORMAT ERROR.\n [%d]", re);
+    
     return 0;
 }
